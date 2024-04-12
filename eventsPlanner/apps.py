0c5a429947b1ead390e3c565118f4175cba1f8f7
Pcosby5 <pcosby50@gmail.com>
2024-04-12 23:05:39 +0200

    deleted other apps

--- eventsPlanner/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventsplannerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eventsPlanner'
--- eventsPlanner/templatetags/my_custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='join_list')
-def join_list(value, arg):
-    return arg.join(value)
