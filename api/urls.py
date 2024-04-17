bf8e50b196886997eac638b553621e3aed338b4f
Pcosby5 <pcosby50@gmail.com>
2024-04-17 12:43:32 +0200

    changed get-profile to profile in the urls

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ from .views import GetProfile
 
 urlpatterns = [
     # path('login/', views.login_api),
-    path('get-profile/', GetProfile.as_view(), name='get_profile'),
+    path('profile/', GetProfile.as_view(), name='profile'),
     path('login/', views.LoginAPIView.as_view()),
     path('update-user/<int:pk>/', views.UpdateUserAPI.as_view()),
     path('user/delete/<int:id>/', UserDeleteView.as_view(), name='user-delete'),
